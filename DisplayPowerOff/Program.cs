7150d42c930c0f0d83af1b802e45e78cd6672a3a
Hiroaki Shimono <salmon2073@gmail.com>
2016-03-24 21:57:04 +0900

    upload files

---
+++ DisplayPowerOff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DisplayPowerOff {
+    class Program {
+        [DllImport("user32.dll")]
+        static extern IntPtr PostMessage(int hWnd, uint Msg, int wParam, int lParam);
+        static void Main(string[] args) {
+            PostMessage(-1, 0x112, 0xf170, 2);
+        }
+    }
+}
